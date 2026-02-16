930982d4db2c1914d40b82a15d176dba2d63e345
bukananda <bukansayawoi@gmail.com>
2026-02-16 14:54:33 +0700

    Menambahkan voice

--- app/inference/camera_infer.py
+++ app/inference/camera_infer.py
@@ -3,6 +3,25 @@ from app.config import ACTIONS, CONFIDENCE_THRESHOLD, TEMPORAL_STABILITY_FRAMES,
 from app.inference.gating import is_camera_active
 from app.inference.filters import EMAFilter
 
+from gtts import gTTS
+import os
+
+def voice(text):
+    print(f"Sedang memproses suara: '{text}'...")
+    
+    # 2. Proses Teks ke Suara Google (Bahasa Indonesia: 'id')
+    tts = gTTS(text=text, lang='id')
+    
+    # 3. Simpan sementara sebagai mp3
+    filename = "temp_voice.mp3"
+    tts.save(filename)
+    
+    # 4. Putar menggunakan mpg123 (perintah sistem Arch)
+    os.system(f"mpg123 -q {filename}")
+    
+    # Opsional: Hapus file setelah diputar
+    os.remove(filename)
+
 class CameraInference:
     def __init__(self, model):
         self.model = model
@@ -12,6 +31,7 @@ class CameraInference:
         self.current_confidence = 0.0
         self.frame_count = 0
         self.filter = EMAFilter(alpha=0.6) # Smooth landmarks
+        self.action_label_now = None
 
     def process_frame(self, landmarks):
         """Process a single frame and return the detected sign.
@@ -51,10 +71,15 @@ class CameraInference:
                     if len(self.predictions) == TEMPORAL_STABILITY_FRAMES:
                         if all(p == action_idx for p in self.predictions):
                             if confidence > CONFIDENCE_THRESHOLD:
-                                self.current_label = ACTIONS[action_idx]
-                                self.current_confidence = confidence
+                                action_label = ACTIONS[action_idx]
+                                if action_label != self.action_label_now:
+                                    voice(ACTIONS[action_idx])
+                                    self.action_label_now = action_label
+                                    self.current_label = ACTIONS[action_idx]
+                                    self.current_confidence = confidence
                             else:
                                 self.current_label = "No sign"
                                 self.current_confidence = 0.0
+                                self.action_label_now = None
 
         return self.current_label, self.current_confidence, is_active, energy
